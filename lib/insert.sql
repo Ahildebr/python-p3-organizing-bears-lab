a87aff455d17dfd2732a742b78470df7688941fb
Aidan Hildebrand <blackbirdgt3rs@gmail.com>
2025-01-31 13:35:43 -0800

    Completes Lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Mr. Chocolate', 20, 'M', 'dark brown', 'grumpy', 1),
+('Rowdy', 10, 'M', 'black', 'friendly', 1),
+('Tabitha', 8, 'F', 'brown', 'sassy', 1),
+('Sargeant Brown', 22, 'M', 'green', 'lazy', 1),
+('Melissa', 20, 'F', 'blue', 'cuddly', 1),
+('Grinch', 6, 'M', 'white', 'grumpy', 1),
+('Wendy', 14, 'F', 'pink', 'friendly', 1),
+(NULL, 12, 'F', 'purple', 'sassy', 1);
